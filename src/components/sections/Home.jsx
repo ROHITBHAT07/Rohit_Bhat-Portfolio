21609164db1c880399145736fa25aad7712d1c4c
Rohit Narayan Bhat <rohitbhat7032@gmail.com>
2025-04-08 12:41:52 +0530

    Updated styles and fixed resume link

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -17,7 +17,7 @@ export const Home = () => {
             applications. My goal is to build solutions that offer both
             exceptional performance and a delightful user experience.
           </p>
-          <div className="flex justify-center space-x-4">
+          <div className="flex flex-wrap justify-center gap-4 mt-4">
             <a
               href="#projects"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] "
@@ -33,7 +33,7 @@ export const Home = () => {
             </a>
 
             <a
-              href="/RohitBhat_CSE.pdf"
+              href="/Rohit_Bhat-Portfolio/RohitBhat_CSE.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] "
@@ -42,7 +42,7 @@ export const Home = () => {
             </a>
 
             <a
-              href="/RohitBhat_CSE.pdf"
+              href="/Rohit_Bhat-Portfolio/RohitBhat_CSE.pdf"
               download
               className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10 "
             >
